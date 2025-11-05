46228338c072488d2ff4566c24cbabdda5e3fa6d
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-05 05:10:58 -0800

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# 5-misol
+for i in range(1,21):
+    if i == 25:
+        print(i)
+
+#6-misol
+sum = 0
+for i in range(1,51):
+    if i % 7 == 0:
+        sum += i
+print(sum)
+
+#7-misol
+for i in range(1,21):
+    print(i)
+    if i == 10:
+        break
+
+#8-misol
+for i in range(1,16):
+    if i % 2 != 0:
+        print(i)
+
+#9-misol
+for i in range(10,0,-1):
+    print(i)
+    if i == 5:
+        break
+
+#10-misol
+for i in range(1,26):
+    if i % 4 == 0:
+        print(i)
+        break
